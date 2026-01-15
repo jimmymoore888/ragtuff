1dec797dff91967756cbc67fab99ac9925c187da
Jimmy W Moore <scaredfallen100@gmail.com>
2026-01-15 01:18:01 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * RagTuff
+ * Early infrastructure for safe, non-custodial wallet connectivity
+ */
+
+export function createRagTuff() {
+  return {
+    name: "RagTuff",
+    version: "0.0.1",
+    status: "initialized"
+  };
+}
